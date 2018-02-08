996bc4d4b59b23b8473105f34c17b5c2b8047f0d
elkouhen <mehdi.elkouhen@gmail.com>
2018-02-08 22:29:38 +0100

    wip

---
+++ src/main/java/com/meltingpoc/events/config/OAuth2AuthenticationConfiguration.java
@@ -0,0 +1,30 @@
+package com.meltingpoc.events.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.web.util.matcher.RequestHeaderRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+@Configuration
+@EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+public class OAuth2AuthenticationConfiguration extends ResourceServerConfigurerAdapter {
+
+    @Bean
+    public RequestMatcher resources() {
+        return new RequestHeaderRequestMatcher("Authorization");
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .requestMatcher(resources())
+                .authorizeRequests()
+                .anyRequest().authenticated();
+    }
+
+}
